7d78a93d84dcfdf70f2a3716ebece30bc6675578
Matt Clarkson <mattyclarkson@gmail.com>
2011-11-11 01:41:50 +0000

    Added a javascript failure message

--- js/index.js
+++ js/index.js
@@ -16,16 +16,41 @@
 (function (window, undefined) {
 	'use strict';
 
+	if (!window) {
+		// Something is seriously fucking wrong.
+		return;
+	}
+
 	// Local copies of the window objects for speed
-	var document	= window.document,
-//		navigator	= window.navigator,
-//		location	= window.location,
-//		parent		= window.parent,
-		console		= window.console,
-		$			= window.$,
-		ko			= window.ko,
-		bb			= window.bb,
-		page		= {};
+	var document			= window.document,
+//		navigator			= window.navigator,
+//		location			= window.location,
+//		parent				= window.parent,
+		console				= window.console,
+		$					= window.$,
+		ko					= window.ko,
+		bb					= window.bb,
+		page				= {};
+
+	// Set up the failure article
+	if (document) {
+		page.javaScriptFail	= document.createElement('article');
+		if (page.javaScriptFail) {
+			page.javaScriptFail.id						= 'javascript-fail';
+			page.javaScriptFail.innerHTML				= '<h1>Hmmm, looks like something broke.</h1><p>It seems the JavaScript had a hiccup on this page.</p><p>You can <a href="mailto:developer@burningboots.co.uk?Subject=JavaScript%20Error%20-%20index.js">email us about this problem</a> or, if you\'re a member of <a href="http://github.com">github</a>, <a href="https://github.com/burning-boots/www.burningboots.co.uk/issues/new">create a new issue for us to solve</a>.</p>';
+			page.javaScriptFail.style.color				= 'black';
+			page.javaScriptFail.style.border			= '1px solid red';
+			page.javaScriptFail.style.backgroundColor	= '#e50';
+			page.javaScriptFail.style.textAlign			= 'center';
+			page.javaScriptFail.style.padding			= '1em';
+			page.javaScriptFail.add						= function () {
+				var main	= document.getElementById('main');
+				if (main) {
+					main.appendChild(this);
+				}
+			};
+		}
+	}
 
 	// Check the needed dependencies
 	if ((window === undefined)		||
@@ -35,6 +60,11 @@
 			(ko === undefined)) {
 		if (console.error) {
 			console.error('BB: Error: You must include jQuery, jQuery Templates, Knockout and the Burning Boots library for the index control Javascript to work.');
+		}
+		if (document) {
+			if (page.javaScriptFail && page.javaScriptFail.add) {
+				page.javaScriptFail.add();
+			}
 		}
 		window.page	= undefined;
 		return;
@@ -47,25 +77,25 @@
 
 	/**	Private view model that can be used with Knockout JS to bind view elements
 	 *	@namespace	Private view model
-	 *	@private
+	 *	@public
 	 *	@since Version 0.1.0
 	 */
 	page.viewModel =
 		{
 			/**	The version of the page control code
-			 *	@private
+			 *	@public
 			 *	@since Version 0.1.2
 			 */
-			version: [0, 1, 2],
+			version: [0, 1, 3],
 
 			/**	The CSS view model code
-			 *	@private
+			 *	@public
 			 *	@since Version 0.1.0
 			 */
 			css:
 				{
 					/**	A list of presentation CSS classes
-					 *	@private
+					 *	@public
 					 *	@since Version 0.1.0
 					 */
 					presentationList: ko.observableArray([
@@ -73,13 +103,13 @@
 					]),
 
 					/**	The selected presentation class
-					 *	@private
+					 *	@public
 					 *	@since Version 0.1.0
 					 */
 					presentation: ko.observable(),
 
 					/**	A list of layout CSS classes
-					 *	@private
+					 *	@public
 					 *	@since Version 0.1.0
 					 */
 					layoutList: ko.observableArray([
@@ -87,7 +117,7 @@
 					]),
 
 					/**	The selected layout CSS class
-					 *	@private
+					 *	@public
 					 *	@since Version 0.1.0
 					 */
 					layout: ko.observable(),
@@ -95,7 +125,7 @@
 		};
 
 	/**	Occurs when the presentation CSS changes
-	 *	@private
+	 *	@public
 	 *	@since Version 0.1.2
 	 */
 	page.viewModel.css.presentation.subscribe(function (value) {
@@ -103,7 +133,7 @@
 	});
 
 	/**	Occurs when the layout CSS changes
-	 *	@private
+	 *	@public
 	 *	@since Version 0.1.2
 	 */
 	page.viewModel.css.layout.subscribe(function (value) {
@@ -147,6 +177,9 @@
 						bb.log.info('Set default presentation');
 					}
 				} catch (exception) {
+					if (page.javaScriptFail && page.javaScriptFail.add) {
+						page.javaScriptFail.add();
+					}
 					bb.log.error('Initialising Page Control Code...FAILED: ' + exception);
 					$(document.documentElement).removeClass('js').addClass('no-js');
 					return;
@@ -162,7 +195,7 @@
 	 *	that can be useful for development.
 	 *	@namespace	Control code for the main index page
 	 *	@exports window.page as index
-	 *	@version 0.1.0
+	 *	@version 0.1.3
 	 */
 	window.page = {};
 }(window));
